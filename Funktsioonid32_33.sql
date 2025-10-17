6a69df59e6054481b305c6f4a5ba9d8c3360a469
melochevsky <milan.petrovski8@gmail.com>
2025-10-17 11:37:53 +0300

    Lopetatud fail 32,33 ulesanned.
    Lisatud:
    
    * Funktsioon fn_GetEmployeeNameById(@Id INT) kryteerimisega
    
    * Funktsioon fn_GetEmployeeNameById(@Id INT) SCHEMABINDING valikut

--- Funktsioonid32_33.sql
+++ Funktsioonid32_33.sql
@@ -36,3 +36,26 @@ END
 -- Käivita funktsiooni
 SELECT dbo.fn_GetEmployeeNameById(1);
 
+
+-- Nüüd muudame funktsiooni ja krüpteerime selle ära
+ALTER FUNCTION fn_GetEmployeeNameById(@Id INT)
+RETURNS NVARCHAR(20)
+WITH ENCRYPTION
+AS
+BEGIN
+RETURN (SELECT FirstName FROM DimEmployee WHERE EmployeeKey = @Id)
+END
+-- Käivita funktsiooni
+SELECT dbo.fn_GetEmployeeNameById(1);
+
+-- Nüüd muuda funktsiooni ja kasuta käsklust WITH SCHEMABINDING valikut
+ALTER FUNCTION fn_GetEmployeeNameById(@Id INT)
+RETURNS NVARCHAR(20)
+WITH SCHEMABINDING
+AS
+BEGIN
+RETURN (SELECT FirstName FROM dbo.DimEmployee WHERE EmployeeKey = @Id)
+END
+-- Käivita funktsiooni
+SELECT dbo.fn_GetEmployeeNameById(1);
+
