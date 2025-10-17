622d39dc0347485c116abd9c975a0a889ff27136
melochevsky <milan.petrovski8@gmail.com>
2025-10-17 11:34:22 +0300

    Lisatud Mitme avaldisega tabeliväärtusega funktsioon
    Mitme avaldisega tabeliväärtusega funktsioonid e multi-statement table valued function (MSTVF):

--- Funktsioonid32_33.sql
+++ Funktsioonid32_33.sql
@@ -10,4 +10,17 @@ Return (Select EmployeeKey, FirstName, CAST(BirthDate AS date) as DOB
 	From dbo.DimEmployee)
 
 SELECT * FROM fn_ILTVF_GetEmployees();
- 
+
+-- Mitme avaldisega tabeliväärtusega funktsioonid e multi-statement table valued function (MSTVF):
+create function fn_mstvf_getemployees()
+returns @Table table (id int,Name nvarchar(20), DOB date)
+as
+begin
+insert into @Table
+select EmployeeKey,FirstName,cast(BirthDate as date)
+from dimEmployee
+return
+end
+--kontroll
+Select * from fn_MSTVF_GetEmployees()
+
