5df4a38f5378f6bad8a472a30775075c6318eabc
melochevsky <milan.petrovski8@gmail.com>
2025-10-17 11:43:28 +0300

    Lisatud fail 35, loodud indeksi, mis naitab päringut: Loome indeksi Salary veerule.

---
+++ indeksidServeris35_38.sql
@@ -0,0 +1,13 @@
+--35
+--Nüüd loome indeksi, mis aitab päringut: Loome indeksi Salary veerule.
+CREATE INDEX IX_DimEmployee_BaseRate
+ON DimEmployee(BaseRate ASC)
+
+--kontroll või execute
+execute sp_help DimEmployee;
+
+--kustuta või drop
+DROP INDEX DimEmployee.IX_DimEmployee_BaseRate
+
+ALTER TABLE DimEmployee
+DROP CONSTRAINT FK_DimEmployee_DimEmployee;
